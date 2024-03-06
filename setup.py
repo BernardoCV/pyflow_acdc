672b5f93700c266c39f2789b0c6c084ccd69e45e
BernardoCV <162305523+BernardoCV@users.noreply.github.com>
2024-03-06 18:02:17 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 10:54:16 2024
+
+@author: BernardoCastro
+"""
+
+from setuptools import setup
+
+setup(name='pyflow_acdc',
+      version='0.1',
+      description='A python-based tool for the design and analysis of hybrid AC/DC grids',
+      url='http://github.com/storborg/funniest',
+      author='Bernardo Castro Valerio',
+      author_email='bernardo.castro@upc.edu',
+      license='BSD',
+      packages=['pyflow_acdc'],
+      python_requires='>=3.8',
+      install_requires=['numpy',
+                        'pandas',
+                        'networkx',
+                        'matplotlib',
+                        'scipy',
+                        'prettytable'],
+      extras_require={"OPF":["pymo", "ipopt"]
+          
+          }
+      )
